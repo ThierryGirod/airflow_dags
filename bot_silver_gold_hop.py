0de4558813626f73364cb754b8ac995253a0cb06
Thierry Girod <thierry.girod@hotmail.com>
2024-09-25 13:16:23 +0000

    add test safe

---
+++ bot_silver_gold_hop.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+# Initialize Spark session
+spark = SparkSession.builder.appName("exampleApp").getOrCreate()
+
+# Create a sample DataFrame
+data = [("Alice", 34), ("Bob", 45), ("Cathy", 29)]
+df = spark.createDataFrame(data, ["Name", "Age"])
+
+df.write.format("delta").saveAsTable("default.test")
+
+# Show DataFrame
+df.show()
+
+# Stop the Spark session
+spark.stop()
