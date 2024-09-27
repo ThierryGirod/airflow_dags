b0dc8d65ef9116d865acdf6a240065e54c183026
Thierry Girod <thierry.girod@hotmail.com>
2024-09-27 09:27:18 +0000

    include delta write

--- bot_silver_gold_hop.py
+++ bot_silver_gold_hop.py
@@ -1,13 +1,17 @@
-from pyspark.sql import SparkSession
+import pyspark
+from delta import *
 
-# Initialize Spark session
-spark = SparkSession.builder.appName("exampleApp").getOrCreate()
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 # Create a sample DataFrame
 data = [("Alice", 34), ("Bob", 45), ("Cathy", 29)]
 df = spark.createDataFrame(data, ["Name", "Age"])
 
-#df.write.format("delta").saveAsTable("default.test")
+df.write.format("delta").saveAsTable("default.test")
 
 # Show DataFrame
 df.show()
